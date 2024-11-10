6f78212025d115fc95a33535efdf0ef5e0a7162a
rafaelbarcarol <rafaelbarcarol@hotmail.com>
2024-11-10 14:52:35 -0300

    Portfolio v1 basic structure

---
+++ script.js
@@ -0,0 +1,75 @@
+const MyApp = (function () {
+  let lastScrollTop = 0;
+
+  function init() {
+    setupAccordion();
+    setupScrollHandler();
+    setupIntersectionObserver();
+    openHamburguerMenu();
+    closeHamburguerMenu();
+    closeMenuOnNavigation();
+  }
+
+  function setupAccordion() {
+    $(".js-toggle-accordion").on("click", function () {
+      $(this).toggleClass("active");
+      $(this).siblings(".js-accordion-content").toggleClass("active");
+    });
+  }
+
+  function setupScrollHandler() {
+    $(window).on("scroll", function () {
+      let scrollTop = $(this).scrollTop();
+
+      if (scrollTop > lastScrollTop) {
+        $(".js-desktop-header").addClass("going-down");
+      } else {
+        $(".js-desktop-header").removeClass("going-down");
+      }
+
+      lastScrollTop = scrollTop;
+    });
+  }
+
+  function setupIntersectionObserver() {
+    const myObserver = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          $(entry.target).addClass("show");
+          myObserver.unobserve(entry.target);
+        }
+      });
+    });
+
+    const elements = $(".hidden-left, .hidden-right");
+    elements.each(function () {
+      myObserver.observe(this);
+    });
+  }
+
+  function openHamburguerMenu() {
+    $(".js-hamburguer-menu").on("click", function () {
+      $(".js-mobile-header-menu").removeClass("inactive");
+    });
+  }
+
+  function closeHamburguerMenu() {
+    $(".js-close-hamburguer").on("click", function () {
+      $(".js-mobile-header-menu").addClass("inactive");
+    });
+  }
+
+  function closeMenuOnNavigation() {
+    $(".js-mobile-menu-item").on("click", function () {
+      $(".js-mobile-header-menu").addClass("inactive");
+    });
+  }
+
+  return {
+    init: init,
+  };
+})();
+
+$(document).ready(function () {
+  MyApp.init();
+});
